b8fd46b33433c4c4a1b85fdadab45d89bf14a436
subhamd@123 <subhamd2022@gift.edu.in>
2024-03-11 21:10:19 +0530

    hurdels with variable height added

---
+++ 100days_of_code/day6/hurdels_with_var.py
@@ -0,0 +1,49 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+# def turn3():
+#      turn_left()
+#      turn_left()
+#      turn_left()
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         while wall_in_front():
+#             turn_left()
+#         move()
+#     turn3()
+#     move()
+#     turn3()      
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+
+
+#          OR
+
+
+# def turn3():
+#      turn_left()
+#      turn_left()
+#      turn_left()
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn3()
+#     move()
+#     turn3()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+          
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+        
+     
+            
