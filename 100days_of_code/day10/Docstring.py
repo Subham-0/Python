e07b305979dca421e735b2795c1901055bcf0050
subhamd@123 <subhamd2022@gift.edu.in>
2024-03-21 19:48:40 +0530

    docstring added

---
+++ 100days_of_code/day10/Docstring.py
@@ -0,0 +1,13 @@
+def Addition(f_no,S_no):
+    """_Additoin of two digits and return it_
+
+    Args:
+        f_no (_type_): _taking f_no input_
+        S_no (_type_): _taking s_no input_
+    """
+    add = f_no+S_no
+    return add
+
+value = Addition(f_no=34,S_no=43)
+print(value)
+
