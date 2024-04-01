62a5fcfbb83bed34e79eecd4669061557274f46b
subhamd@123 <subhamd2022@gift.edu.in>
2024-04-01 10:35:42 +0530

    Day_17 added

--- Interface Software/Day_17/Ex1.py
+++ Interface Software/Day_17/Ex1.py
@@ -1,30 +1,66 @@
-                # Multi-Threading
-"""
-    There are two type of Proccess
-    1-Thread based multitasking
-    2-Process based multitasking
+# Multi-Threading
 
-Thread based multitasking:
-    In case of thread based multitasking there are many process and at the same time all the process are working , but the origin of the process are from the main thread , which means from a program which contains the main thread and or child thread will come from the main thread
-    
-    Every programe is run by a frame which is called as the main thread
-    
-    Every main thread has a thread priority and by default it is 5.
-    
-    Every thread goes to a thread suduler which is the part of the opraation system and thread seduler look for the prority of the threads , as all the priroty are 5 then thread seduler goes for a first come first serve and allow the thread to work on the process
-    
-    Child thread and its life cycle:
-        every main thread once it created autometically goes to a life cycle , so that thread can start and can run the main thread (there is no need for the life cycle of the main thread), where as child thread has the life cycle stage
-        
-        1-create the child thread
-        2-after creation of the child thread then call a method call start so that the child thread is aivialable to thread suduler and call a method called run , so that thread seduler will allow a child
-        
-    How to create the child thred
-        1-child thread can be created when we are extending  to a class called thread 
-        2-thread belongs to a module called as a thread 
-        3-once the thread is created by making the object of the class then we can  call to the start method which autometically call to run method
-        4-every program is run by a main thread and if you want to know the thread name . That we can also find out the name of the main thread
-    """
+"""
+There are two types of Processes:
+1. Thread-based multitasking
+2. Process-based multitasking
+
+Thread-based multitasking:
+In thread-based multitasking, many processes run concurrently, all originating from the main thread of a program.
+
+Every program is run by a framework, which is called the main thread. Each main thread has a default thread priority, typically set to 5.
+
+Threads are managed by a thread scheduler, a component of the operating system. The thread scheduler determines the execution order of threads based on their priorities. If threads have the same priority, the scheduler typically follows a "first come, first served" approach.
+
+Child thread and its life cycle:
+Every main thread, once created, automatically goes through a life cycle, enabling the thread to start and execute the main program. In contrast, a child thread has its own life cycle stages:
+
+1. Create the child thread.
+2. After creating the child thread, call the "start" method. This makes the child thread available to the thread scheduler. Then, call the "run" method to execute the task assigned to the child thread.
+
+How to create a child thread:
+1. To create a child thread, we extend a class called "Thread".
+2. The "Thread" class is part of the "threading" module in Python.
+3. Once we create an object of the "Thread" class, we call the "start" method, which automatically invokes the "run" method.
+4. Every program is initially run by the main thread. To retrieve the name of the main thread, we can access its name property."""
+
+# For example, to create and start a child thread in Python:
+
+import threading
+
+# Define a function to be executed by the child thread
+def print_numbers():
+    for i in range(1, 6):
+        print("Child Thread:", i)
+
+# Create a child thread
+child_thread = threading.Thread(target=print_numbers)
+
+# Start the child thread
+child_thread.start()
+
+# Main thread continues its execution
+for i in range(1, 4):
+    print("Main Thread:", i)
+
+# Wait for the child thread to complete
+child_thread.join()
+
+# Output:
+# Child Thread: 1
+# Main Thread: 1
+# Child Thread: 2
+# Child Thread: 3
+# Main Thread: 2
+# Child Thread: 4
+# Main Thread: 3
+# Child Thread: 5
+
+"""This example demonstrates how the child thread executes concurrently with the main thread, printing numbers in parallel.
+
+When working with threads, it is important to use synchronization techniques such as locks and semaphores to prevent conflicts and ensure data integrity in multi-threaded programs.
+"""
+
     
 from threading import *
 def show():
---
+++ Interface Software/Day_17/Ex5.py
@@ -0,0 +1,6 @@
+# task = prepare a tea by us and following are the task 
+# 1- switch on the stove
+# 2-put the boiling pot and the water to hot for two minutes 
+# 3-put the dust and sugar and boild for 2minutes
+# 4-then filter it
+# write the example using class and run method
