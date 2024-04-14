e7a38118fe4e9ba1eafd6c3ed29998eed1fa23d0
subhamd@123 <subhamd2022@gift.edu.in>
2024-04-14 23:46:34 +0530

    day24 added

---
+++ 100days_of_code/day24/main.py
@@ -0,0 +1,19 @@
+# file = open("my_file.txt")
+
+# contents = file.read()
+# print(contents)
+
+# file.close()
+
+# instaded of manually close the open file we can write like this to close the file autometically
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+    
+#remove the previous data write new
+with open("my_file.txt", mode='w') as file:
+    file.write("\nNew text added")
+
+#write new data with the existing data    
+with open("my_file.txt", mode='a') as file:
+    file.write("\nNew text added")
