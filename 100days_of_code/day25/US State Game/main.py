a5a33a1606c1deeec6a7487fbacd2d7116eb413e
subhamd@123 <subhamd2022@gift.edu.in>
2024-04-23 14:55:52 +0530

    update code (list comprehension)

--- 100days_of_code/day25/US State Game/main.py
+++ 100days_of_code/day25/US State Game/main.py
@@ -19,10 +19,11 @@ while len(guessed_state) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_state)}/50 States Correct", prompt="What's another state's name ?").title()
     
     if answer_state == "Exit":
-        missing_state = []
-        for state in all_states:
-            if state not in guessed_state:
-                missing_state.append(state)
+        missing_state = [state for state in all_states if state not in guessed_state] #list comprehension
+        # missing_state = []
+        # for state in all_states:
+        #     if state not in guessed_state:
+        #         missing_state.append(state)
         new_data = pandas.DataFrame(missing_state)
         new_data.to_csv("states_to_learn.csv")
         break
